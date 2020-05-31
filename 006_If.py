206b8704ba0bb6a12ca342fb4cabc0dbbcfd15ce
vs-create <vladis.shepel@gmail.com>
2020-05-31 20:03:20 +0300

    005

---
+++ 006_If.py
@@ -0,0 +1,21 @@
+a = int(input("'Введите число A: "))
+b = int(input("'Введите число B: "))
+c = int(input("'Введите число C: "))
+d = a+b
+e = b-c
+
+if a>b: print("Свершилось!")
+if b>a: print("Да ну!")
+if a==b: print ("А если так?", "A=",d, "B=",e)
+a=d
+b=e
+if a>b: print("Свершилось!")
+if b>a: print("Да ну!")
+
+
+
+
+
+
+
+
